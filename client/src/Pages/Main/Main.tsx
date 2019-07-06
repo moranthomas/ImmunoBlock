222fd6ee916268aa37502ef152352452506553b0
Bernardo Vieira <bernardo5@live.com.pt>
2019-07-06 20:19:38 +0100

    add main screen

--- client/src/Pages/Main/Main.tsx
+++ client/src/Pages/Main/Main.tsx
@@ -5,6 +5,7 @@ import SimpleStorageContract from '../../contracts/SimpleStorage.json';
 import getWeb3 from '../../utils/getWeb3';
 
 import Navbar from '../../Components/Navbar/Navbar';
+import './main.css';
 import Quiz from './Quiz';
 
 
@@ -15,6 +16,7 @@ const MainContent = styled.div`
 `;
 
 interface IMainState {
+    currentView: string;
 }
 class Main extends Component<{}, IMainState> {
     /**
@@ -22,20 +24,76 @@ class Main extends Component<{}, IMainState> {
      */
     constructor(props: any) {
         super(props);
-        this.state = {};
+        this.state = {
+            currentView: 'main',
+        };
+    }
+
+    public handleClick = (event: any) => {
+        this.setState({ currentView: event.currentTarget.dataset.tag });
     }
 
     /**
      * @ignore
      */
     public render() {
+        const { currentView } = this.state;
         return (
             <>
                 <Navbar />
                 <MainContent>
-                    <Quiz />
+                    {currentView === 'quiz' ? <Quiz /> : this.loadMainCards()}
                 </MainContent>
             </>
+        );
+    }
+
+    private loadMainCards() {
+        return (
+            <table>
+                <tbody>
+                    <tr>
+                        <td data-tag="quiz">
+                            <div className="card" data-tag="quiz" onClick={this.handleClick}>
+                                <div className="card-image">
+                                    <figure className="image is-4by3">
+                                        <img
+                                            src="https://bulma.io/images/placeholders/1280x960.png"
+                                            alt="Placeholder image"
+                                        />
+                                    </figure>
+                                </div>
+                                <div className="card-content">
+                                    <div className="content">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Phasellus nec iaculis mauris. <strong>Answer Quiz</strong>.
+                                    <br />
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <div className="card">
+                                <div className="card-image">
+                                    <figure className="image is-4by3">
+                                        <img
+                                            src="https://bulma.io/images/placeholders/1280x960.png"
+                                            alt="Placeholder image"
+                                        />
+                                    </figure>
+                                </div>
+                                <div className="card-content">
+                                    <div className="content">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Phasellus nec iaculis mauris. <strong>View Statistics</strong>.
+                                    <br />
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         );
     }
 }
