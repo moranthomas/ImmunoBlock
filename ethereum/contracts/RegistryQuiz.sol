f379327441ecbfc121b7840748d3315ebcf7b8fc
Bernardo Vieira <bernardo5@live.com.pt>
2019-08-03 21:01:13 +0100

    update dependencies

--- ethereum/contracts/RegistryQuiz.sol
+++ ethereum/contracts/RegistryQuiz.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.7;
 
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 
 contract RegistryQuiz is Ownable {
